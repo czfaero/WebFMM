69e9e0e7a772e3717a8670028aca55abec5a9644
Chen Zaifeng <79635397+czfaero@users.noreply.github.com>
2023-02-24 16:08:25 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import { NodeLinkRenderer } from './NodeLinkRenderer';
 
 import { GetNodes, GetLinks, GetNodeColors } from './diagrams/MatrixMarketLoader'
 
+import { FMMSolver } from './FMMSolver';
 
 async function main() {
     const canvas = document.querySelector("canvas") as HTMLCanvasElement;
     const nodes = await GetNodes();
     const links = await GetLinks();
     const nodeColors = await GetNodeColors();
+
+    const solver = new FMMSolver();
 
 
     const renderer = new NodeLinkRenderer();
