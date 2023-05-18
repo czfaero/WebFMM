0c1fe80a16c514b37bc1ac144ed3f51bbd1661b3
Chen Zaifeng <79635397+czfaero@users.noreply.github.com>
2023-05-18 13:28:04 +0900

    abstract kernel

--- src/kernels/kernel.ts
+++ src/kernels/kernel.ts
@@ -1,4 +1,6 @@
 export interface IKernel {
+    debug: boolean;
+    accelBuffer: Float32Array;
     Init: (particleBuffer: Float32Array) => Promise<void>;
-    p2p: (numBoxIndex: number, interactionList: any, numInteraction: any, particleOffset: any) => void
+    p2p: (numBoxIndex: number, interactionList: any, numInteraction: any, particleOffset: any) => Promise<void>;
 }
