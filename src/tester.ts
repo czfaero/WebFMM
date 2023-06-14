ee68c526e08e828ae10acca433eaa75ffa75397b
Chen Zaifeng <79635397+czfaero@users.noreply.github.com>
2023-06-14 17:48:10 +0900

    Update tester.ts

--- src/tester.ts
+++ src/tester.ts
@@ -4,6 +4,7 @@ export class Tester {
     constructor() {
     }
     async Test(instance: FMMSolver) {
+        instance.kernel.debug = true;
         instance.setBoxSize();
         instance.setOptimumLevel();
         console.log("maxLevel: " + instance.maxLevel);
@@ -30,13 +31,20 @@ export class Tester {
         instance.getInteractionListP2P(numBoxIndex, numLevel);
 
         VerifyIntIntBuffer("data-p2p-list.bin", instance.interactionList);
-        //     bodyAccel.fill(0);
-        //     kernel.p2p(numBoxIndex);
+        
+        await instance.kernel.Init(instance.particleBuffer);
+
+        let time_p2p = performance.now();
+        await instance.kernel.p2p(numBoxIndex, instance.interactionList, instance.numInteraction, instance.particleOffset);
+        time_p2p = performance.now() - time_p2p;
+        console.log(`time p2p: ${time_p2p.toFixed(2)} ms`);
+
+        VerifyFloatBuffer("data-p2p.bin", instance.kernel.accelBuffer);
     }
 }
 
 function CompareNumber(a: number, b: number) {
-    return Math.abs(a - b) < 0.00001
+    return Math.abs(a - b) < 0.001
 }
 function p2str(data: Float32Array, i: number) {
     const xyzw = Array.from(data.subarray(i * 4, i * 4 + 4)).map(x => x.toFixed(2)).join(' ');
@@ -70,6 +78,40 @@ async function VerifyParticleBuffer(name: string, data: Float32Array) {
         throw "Failure: " + name;
     }
 }
+
+async function VerifyFloatBuffer(name: string, data: Float32Array) {
+    const rawData = await (await fetch(name)).arrayBuffer();
+    const expect = new Float32Array(rawData);
+
+    console.log("bin size:" + expect.length)
+    console.log(`Sample expect: ${Array.from(expect.subarray(0, 4)).map(n => n.toFixed(3)).join(' ')}`)
+    console.log(`Sample buffer: ${Array.from(data.subarray(0, 4)).map(n => n.toFixed(3)).join(' ')}`)
+
+    if (data.length != expect.length) {
+        console.log(data);
+        throw `${name} size: ${data.length}!=${expect.length}`;
+    }
+    let error_count = 0;
+    for (let i = 0; i < data.length; i++) {
+        const r = CompareNumber(expect[i], data[i]);
+        if (!r) {
+            error_count++;
+            console.log(`[${i}]Expect: ${expect[i]} | Got: ${data[i]}`);
+            if (error_count > 10) {
+                break;
+            }
+        }
+    }
+    if (error_count == 0) {
+        console.log("Success: " + name);
+    }
+    else {
+        console.log(expect);
+        console.log(data);
+        throw "Failure: " + name;
+    }
+}
+
 
 async function VerifyIntBuffer(name: string, data: Int32Array) {
     const rawData = await (await fetch(name)).arrayBuffer();
