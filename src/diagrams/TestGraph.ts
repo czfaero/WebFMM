84cb8d12d165b6bbb78048ce3f290c56a2297be2
Chen Zaifeng <79635397+czfaero@users.noreply.github.com>
2024-04-16 15:39:49 +0900

    Update TestGraph.ts

--- src/diagrams/TestGraph.ts
+++ src/diagrams/TestGraph.ts
@@ -1,16 +1,16 @@
-const a = 10, b = 10, c = 4;
+const size_a = 10, size_b = 10, size_c = 4;
 const interval = 0.1;
-const count = a * b * c;
+const count = size_a * size_b * size_c;
 export const GetNodes = function () {
 
-    const baseX = -Math.floor(a / 2) * interval,
-        baseY = -Math.floor(b / 2) * interval,
-        baseZ = -Math.floor(c / 2) * interval;
+    const baseX = -Math.floor(size_a / 2) * interval,
+        baseY = -Math.floor(size_b / 2) * interval,
+        baseZ = -Math.floor(size_c / 2) * interval;
     const iterable = (function* () {
 
-        for (let x = 0; x < a; x++)
-            for (let y = 0; y < b; y++)
-                for (let z = 0; z < c; z++) {
+        for (let x = 0; x < size_a; x++)
+            for (let y = 0; y < size_b; y++)
+                for (let z = 0; z < size_c; z++) {
                     yield x + baseX;
                     yield y + baseY;
                     yield z + baseZ;
@@ -23,26 +23,24 @@ export const GetNodes = function () {
 }
 export const GetLinks = function () {
     const iterable = (function* () {
-        for (let x = 0; x < a; x++)
-            for (let y = 0; y < b; y++)
-                for (let z = 0; z < c - 1; z++) {
-                    let i = x * b * c + y * c + z;
+        for (let x = 0; x < size_a; x++)
+            for (let y = 0; y < size_b; y++)
+                for (let z = 0; z < size_c - 1; z++) {
+                    let i = x * size_b * size_c + y * size_c + z;
                     yield i;
                     yield i + 1;
                 }
-        for (let x = 0; x < a; x++)
-            for (let y = 0; y < b - 1; y++) {
-                let i = x * b * c + y * c;
+        for (let x = 0; x < size_a; x++)
+            for (let y = 0; y < size_b - 1; y++) {
+                let i = x * size_b * size_c + y * size_c;
                 yield i;
-                yield i + c;
-
+                yield i + size_c;
             }
-
-            for (let x = 0; x < a-1; x++){
-                let i = x * b * c;
-                yield i;
-                yield i + b*c;
-            }
+        for (let x = 0; x < size_a - 1; x++) {
+            let i = x * size_b * size_c;
+            yield i;
+            yield i + size_b * size_c;
+        }
 
     })();
 
@@ -52,17 +50,19 @@ export const GetLinks = function () {
     return links;
 }
 export const GetNodeColors = function () {
-    let r = 1, g = 0, b = 0, t;
+    let r = 1, g = 0, b = 0, t = 0;
     const iterable = (function* () {
-        for (let i = 0; i < count; i++) {
-            yield r;
-            yield g;
-            yield b;
-            t = g;
-            g = r;
-            r = b;
-            b = t;
-        }
+        for (let x = 0; x < size_a; x++)
+            for (let y = 0; y < size_b; y++)
+                for (let z = 0; z < size_c; z++) {
+                    yield r;
+                    yield g;
+                    yield b;
+                    t = g;
+                    g = r;
+                    r = b;
+                    b = t;
+                }
     })();
     const colors = new Float32Array(iterable);
     console.log(colors.length);
