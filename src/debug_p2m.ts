703164a704a53bd66377938ba0eda72f7a7a9d30
Chen Zaifeng <79635397+czfaero@users.noreply.github.com>
2024-11-14 10:33:25 +0900

    debug p2m: new

--- src/debug_p2m.ts
+++ src/debug_p2m.ts
@@ -1,3 +1,4 @@
+import { CalcALP } from "./AssociatedLegendrePolyn";
 import { FMMSolver } from "./FMMSolver";
 import { TreeBuilder } from "./TreeBuilder";
 const PI = 3.14159265358979323846;
@@ -26,7 +27,7 @@ export function debug_p2m(core: FMMSolver, box_id) {
         boxMinY: core.tree.boxMinY,
         boxMinZ: core.tree.boxMinZ,
     }
-    const r = debug_p2m_shader(box_id, tree.boxIndexFull[box_id],
+    const r = debug_p2m_shader2(box_id, tree.boxIndexFull[box_id],
         {
             factorial: factorial,
             uniforms: uniforms,
@@ -48,6 +49,7 @@ const threadsPerGroup = 64;
 //var<workgroup> particle: vec4f;
 // numCoefficients= this.numExpansions * (this.numExpansions + 1) / 2;
 function debug_p2m_shader(box: number, index, buffers) {
+    console.log("old p2m")
     console.log(buffers)
     function u32(x) { return Math.floor(x); }
     function f32(x) { return x; }
@@ -128,8 +130,8 @@ function debug_p2m_shader(box: number, index, buffers) {
         return vec4f(particleBuffer[i * 4], particleBuffer[i * 4 + 1], particleBuffer[i * 4 + 2], particleBuffer[i * 4 + 3]);
     }
 
-    var ng = new Int32Array(threadsPerGroup);
-    var mg = new Int32Array(threadsPerGroup);
+    let ng = new Int32Array(threadsPerGroup);
+    let mg = new Int32Array(threadsPerGroup);
     for (let n = 0; n < numExpansions; n++) {
         for (let m = 0; m <= n; m++) {
             let nms = n * (n + 1) / 2 + m;
@@ -180,10 +182,144 @@ function debug_p2m_shader(box: number, index, buffers) {
             resultBuffer[thread * 2] += particle.w * YnmReal[nm] * ere;
             resultBuffer[thread * 2 + 1] += particle.w * YnmReal[nm] * eim;
         }
-
-
     }
     return resultBuffer;
-
 }
 // one thread for a complex number of coef
+
+
+function debug_p2m_shader2(box_id: number, index: number, buffers: any) {
+    function u32(x) { return Math.floor(x); }
+    function f32(x) { return x; }
+    function i32(x) { return Math.floor(x); }
+    function vec3f(x, y, z) { return { x: x, y: y, z: z }; }
+    function vec4f(x, y, z, w) { return { x: x, y: y, z: z, w: w }; }
+    function vec2f(x, y) { return { x: x, y: y }; }
+    function dot(a, b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+    const sqrt = Math.sqrt;
+    function inverseSqrt(x) { return 1 / Math.sqrt(x); }
+    const abs = Math.abs;
+    const pow = Math.pow;
+    const acos = Math.acos;
+    const atan = Math.atan;
+    const cos = Math.cos;
+    const sin = Math.sin;
+    function vec3_add(arr) {
+        let x = 0, y = 0, z = 0;
+        for (const v of arr) { x += v.x; y += v.y; z += v.z; }
+        return { x: x, y: y, z: z }
+    }
+    function vec3_minus(v) { return { x: -v.x, y: -v.y, z: -v.z } }
+
+
+    function unmorton(boxIndex) {
+        var mortonIndex3D = [0, 0, 0];
+
+        var n = boxIndex;
+        var k = 0;
+        var i = 0;
+        while (n != 0) {
+            let j = 2 - k;
+            mortonIndex3D[j] += (n % 2) * (1 << i);
+            n >>= 1;
+            k = (k + 1) % 3;
+            if (k == 0) { i++; }
+        }
+        return {
+            x: mortonIndex3D[1],
+            y: mortonIndex3D[2],
+            z: mortonIndex3D[0]
+        };
+    }
+    function cart2sph(d) {
+        var r = sqrt(d.x * d.x + d.y * d.y + d.z * d.z) + eps;
+        var theta = acos(d.z / r);
+        var phi;
+        if (abs(d.x) + abs(d.y) < eps) {
+            phi = 0;
+        }
+        else if (abs(d.x) < eps) {
+            phi = d.y / abs(d.y) * PI * 0.5;
+        }
+        else if (d.x > 0) {
+            phi = atan(d.y / d.x);
+        }
+        else {
+            phi = atan(d.y / d.x) + PI;
+        }
+        return vec3f(r, theta, phi);
+    }
+
+    const uniforms = buffers.uniforms;
+    const numExpansions = uniforms.numExpansions;
+    const numExpansions2 = numExpansions * numExpansions;
+    const numCoefficients = numExpansions * (numExpansions + 1) / 2;
+    const resultBuffer = new Float32Array(numExpansions2 * 2);
+    const boxSize = uniforms.boxSize;
+    const particleOffset = buffers.particleOffset;
+    const factorial = buffers.factorial;
+
+    // let start = particleOffset[box * 2];
+    // let end = particleOffset[box * 2 + 1];
+    let start = particleOffset[0][box_id];
+    let end = particleOffset[1][box_id];
+
+    const particleBuffer: Float32Array = buffers.particleBuffer;
+    function getParticle(i) {
+        return vec4f(particleBuffer[i * 4], particleBuffer[i * 4 + 1], particleBuffer[i * 4 + 2], particleBuffer[i * 4 + 3]);
+    }
+
+    let index3D = unmorton(u32(index));
+    let boxMin = vec3f(uniforms.boxMinX, uniforms.boxMinY, uniforms.boxMinZ);
+    let boxCenter = vec3_add([index3D, vec3f(0.5 * boxSize, 0.5 * boxSize, 0.5 * boxSize), boxMin]);
+
+
+    let ng = new Int32Array(threadsPerGroup);
+    let mg = new Int32Array(threadsPerGroup);
+    for (let n = 0; n < numExpansions; n++) {
+        for (let m = 0; m <= n; m++) {
+            let nms = n * (n + 1) / 2 + m;
+            ng[nms] = n;
+            mg[nms] = m;
+        }
+    }
+
+    function thread(thread_id: number) {
+        let M_real = 0, M_imag = 0;
+        let n = ng[thread_id], m = mg[thread_id];
+
+        // maxParticlePerBox let particleIndex = start + i;
+        for (let particleIndex = start; particleIndex <= end; particleIndex++) {
+            let particle = getParticle(particleIndex);
+            let dist = vec3f(particle.x - boxCenter.x, particle.y - boxCenter.y, particle.z - boxCenter.z);
+            let r = cart2sph(dist);
+            let rho = r.x; let alpha = r.y; let beta = r.z;
+
+            // only thread_0_0
+            const Pnm = CalcALP(numExpansions, cos(alpha));// shared
+            const rho_n = new Float32Array(numExpansions);//shared
+            for (let i = 0, v = 1; i < rho_n.length; i++) {
+                rho_n[i] = v;
+                v = v * rho;
+            }
+
+            // sync
+            let i = n * (n + 1) / 2 + m;
+            const C = particle.w * rho_n[n] * sqrt(factorial[n - m] / factorial[n + m]) * Pnm[i];
+            M_real += C * cos(-m * beta);
+            M_imag += C * sin(-m * beta);
+            // sync
+        }
+        let i = n * n + n + m;
+        resultBuffer[i * 2] = M_real;
+        resultBuffer[i * 2 + 1] = M_imag;
+        i = n * n + n - m;//共轭
+        resultBuffer[i * 2] = M_real;
+        resultBuffer[i * 2 + 1] = -M_imag;
+    }
+
+    for (let thread_id = 0; thread_id < threadsPerGroup; thread_id++) {
+        thread(thread_id);
+    }
+    return resultBuffer;
+}
